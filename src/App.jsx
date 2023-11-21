4f68b8d025720d253c32a4cd82326c7adcc6bf5a
Alejandro Leonian <alejandromleonian@gmail.com>
2023-11-21 16:28:35 -0500

    i think i like this version better

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Menu } from './Components/Menu';
 import './index.css'
+// script.js
+import "nes.css/css/nes.min.css";
 
 function App() {
 
--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,16 +1,30 @@
 import React from "react";
-import { Button, Colors, Container, Text } from "nes-ui-react";
+// import { Button, Colors, Container, Text } from "nes-ui-react";
 
-function emptyCallback() {
-
-}
 export function Menu() {
     return (
-        <Container title='Menu' className="menu">
-            <Button size="large" className="menuItem" color="primary" onClick={emptyCallback}>Projects</Button>
-            <Button size="large" className="menuItem" color="warning" onClick={emptyCallback}>About</Button>
-            <Button size="large" className="menuItem" color="error" onClick={emptyCallback}>Contact</Button>
-            <Button size="large" className="menuItem" color="success" onClick={emptyCallback}>Links</Button>
-        </Container>
+        <>
+            <div className="menu nes-container">
+                <button type="button" className="nes-btn is-primary is-icon">Projects
+                <i className="nes-icon star is-small"></i>
+                </button>
+                <button type="button" className="nes-btn is-success">About</button>
+                <button type="button" className="nes-btn is-warning">Contact</button>
+                <button type="button" className="nes-btn is-error">Links</button>
+            </div>
+        </>
+        // <section className="showcase">
+        //     <section className="menu nes-container with-title">
+        //         <h3 className="title">Buttons</h3>
+        //         <div id="buttons" className="item">
+        //             <a className="nes-btn" href="#">Normal</a>
+        //             <button type="button" className="nes-btn is-primary">Primary</button>
+        //             <button type="button" className="nes-btn is-success">Success</button>
+        //             <button type="button" className="nes-btn is-warning">Warning</button>
+        //             <button type="button" className="nes-btn is-error">Error</button>
+        //             <button type="button" className="nes-btn is-disabled">Disabled</button>
+        //         </div>
+        //     </section>
+        // </section>
     )
 }
