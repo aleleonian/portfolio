3fe3e404bc8179632f298274127537b8665fe70b
Alejandro Leonian <alejandromleonian@gmail.com>
2023-12-07 23:56:44 -0500

    added images

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -8,6 +8,7 @@ export function Intro() {
                 <p className="nes-balloon from-left is-dark">Hi! My name is Alejandro and i'm a Full Stack Javascript Developer.</p>
                 <p className="nes-balloon from-right ">I started building websites when all you needed was HTML and Google did not exist :D</p>
                 <p className="nes-balloon from-left is-dark">Currently I use React.js, Node.js, HTML, CSS, Javascript, MySql and MongoDB among other tools.</p>
+                <img src="/public/Images/smiley.png" width="50"/>
             {/* </div> */}
         </div>
     )
